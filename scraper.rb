90f8768d0ad6c25a14fdc96440a0683d8664ef95
Tony Bowden <tony@tmtm.com>
2016-05-09 09:43:38 +0100

    Update for Term 6

--- scraper.rb
+++ scraper.rb
@@ -31,22 +31,21 @@ end
 
 def scrape_term(url, term)
   page = noko(url)
-  rows = page.css('div#tablez').xpath('//table[.//th[contains(.,"Fullname")]]/tr[td[contains(.,"Questions")]]')
-  rows.each do |mem|
+  page.css('table#example tbody tr').each do |mem|
     tds = mem.css('td')
     link = tds[1].css('a/@href').text
     data = { 
-      id: tds[0].css('img/@src').text.split('/').last[/^(\d+)/, 1],
+      id: link.split('/').last,
       photo: tds[0].css('img/@src').text,
-      name: tds[1].text.strip, # but overwritten if we have parts on their MP page
-      sort_name: tds[1].text.strip,
-      party: tds[2].text.strip,
+      name: tds[1].text.strip.sub(/^Hon.? /,''), 
+      area: tds[2].text.strip,
+      party: tds[3].text.strip,
+      member_type: tds[4].text.strip,
       term: term,
       source: link,
     }
     %i(photo source).each { |i| data[i] = URI.join(url, URI.encode(data[i])).to_s unless data[i].to_s.empty? }
     data = data.merge(scrape_person(data[:source]))
-    # puts data
     ScraperWiki.save_sqlite([:id, :term], data)
   end
   
@@ -56,25 +55,14 @@ end
 
 def scrape_person(url)
   page = noko(url)
-  box = page.xpath('//div[@id="tablez"]//table[.//th[contains(.,"GENERAL")]]')
+  box = page.css('div#divToPrint')
   data = { 
-    salutation: box.xpath('.//td[.//strong[contains(.,"Salutation")]]/following-sibling::td[1]').text.tidy,
-    given_name: box.xpath('.//td[.//strong[contains(.,"First Name")]]/following-sibling::td[1]').text.tidy,
-    middle_name: box.xpath('.//td[.//strong[contains(.,"Middle Name")]]/following-sibling::td[1]').text.tidy,
-    family_name: box.xpath('.//td[.//strong[contains(.,"Last Name")]]/following-sibling::td[1]').text.tidy,
-    member_type: box.xpath('.//td[.//strong[contains(.,"Member Type")]]/following-sibling::td[1]').text.tidy,
-    area: box.xpath('.//td[.//strong[contains(.,"Constituent")]]/following-sibling::td[1]').text.tidy,
-    email: box.xpath('.//td[.//strong[contains(.,"E-mail")]]/following-sibling::td[1]').text.tidy,
-    birth_date: date_from(box.xpath('.//td[.//strong[contains(.,"Date of Birth")]]/following-sibling::td[1]').text.tidy),
+    phone: box.xpath('.//span[@class="item"][contains(.,"Phone")]//following-sibling::text()').text.tidy,
+    email: box.xpath('.//span[@class="item"][contains(.,"Email")]//following-sibling::text()').text.tidy,
+    birth_date: date_from(box.xpath('.//span[@class="item"][contains(.,"Date of Birth")]//following-sibling::text()').text.tidy),
   }
-  data[:name] = %i(given_name middle_name family_name).map { |i| data[i] }.join " "
-  return data[:name].tidy.empty? ? {} : data
 end
 
 
-url = 'http://www.parliament.go.tz/index.php/members/memberslist/all/all/%i'
-
-(2..4).reverse_each do |term|
-  puts term
-  scrape_term(url % term, term)
-end
+url = 'http://www.parliament.go.tz/mps-list'
+scrape_term(url, 6)
