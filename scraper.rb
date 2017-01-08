b2db770bcd007944a99f95afe01e8a1bf4ea2ac8
Tony Bowden <tony@tmtm.com>
2017-01-08 09:03:27 +0000

    Turn on archiving

--- scraper.rb
+++ scraper.rb
@@ -7,8 +7,9 @@ require 'pry'
 require 'scraped'
 require 'scraperwiki'
 
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '.cache'
+require 'scraped_page_archive/open-uri'
 
 @BASE = 'http://www.parliament.go.tz'
 
