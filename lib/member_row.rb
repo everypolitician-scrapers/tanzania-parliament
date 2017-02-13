8e4230c3126ff8bf4969e6d0d26eca5d0726c9c3
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-13 16:05:32 +0000

    Add MemberRow fragment

---
+++ lib/member_row.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+require 'scraped'
+
+class MemberRow < Scraped::HTML
+  field :id do
+    link.split('/').last
+  end
+
+  field :photo do
+    URI.encode(noko[0].css('img/@src').text)
+  end
+
+  field :name do
+    noko[1].text.strip.sub(/^Hon.? /, '')
+  end
+
+  field :area do
+    noko[2].text.strip
+  end
+
+  field :party do
+    noko[3].text.strip
+  end
+
+  field :member_type do
+    noko[4].text.strip
+  end
+
+  field :source do
+    link
+  end
+
+  private
+
+  def link
+    noko[1].css('a/@href').text
+  end
+end
